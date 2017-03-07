7efcc22df52fadaa826e6a9ecd3cf2ccfc3a44de
DenDrummer <muzikant96@gmail.com>
2017-03-07 15:22:00 +0100

    MainMenu layout done

--- MainMenuFormsApp/Form1.Designer.cs
+++ MainMenuFormsApp/Form1.Designer.cs
@@ -32,6 +32,10 @@
             this.AsyncForms1Button = new System.Windows.Forms.Button();
             this.AsyncFruitCountingButton = new System.Windows.Forms.Button();
             this.EAPButton = new System.Windows.Forms.Button();
+            this.FakeConsoleAppButton = new System.Windows.Forms.Button();
+            this.LocksButton = new System.Windows.Forms.Button();
+            this.ParallelCodeButton = new System.Windows.Forms.Button();
+            this.QuitButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // APMButton
@@ -82,15 +86,68 @@
             this.EAPButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.EAPButton.UseVisualStyleBackColor = true;
             // 
+            // FakeConsoleAppButton
+            // 
+            this.FakeConsoleAppButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.FakeConsoleAppButton.Location = new System.Drawing.Point(12, 164);
+            this.FakeConsoleAppButton.Name = "FakeConsoleAppButton";
+            this.FakeConsoleAppButton.Size = new System.Drawing.Size(178, 32);
+            this.FakeConsoleAppButton.TabIndex = 0;
+            this.FakeConsoleAppButton.Text = "&5 - FakeConsoleApp";
+            this.FakeConsoleAppButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.FakeConsoleAppButton.UseVisualStyleBackColor = true;
+            // 
+            // LocksButton
+            // 
+            this.LocksButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LocksButton.Location = new System.Drawing.Point(12, 202);
+            this.LocksButton.Name = "LocksButton";
+            this.LocksButton.Size = new System.Drawing.Size(178, 32);
+            this.LocksButton.TabIndex = 0;
+            this.LocksButton.Text = "&6 - Locks";
+            this.LocksButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.LocksButton.UseVisualStyleBackColor = true;
+            // 
+            // ParallelCodeButton
+            // 
+            this.ParallelCodeButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ParallelCodeButton.Location = new System.Drawing.Point(12, 240);
+            this.ParallelCodeButton.Name = "ParallelCodeButton";
+            this.ParallelCodeButton.Size = new System.Drawing.Size(178, 32);
+            this.ParallelCodeButton.TabIndex = 0;
+            this.ParallelCodeButton.Text = "&7 - ParallelCode";
+            this.ParallelCodeButton.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.ParallelCodeButton.UseVisualStyleBackColor = true;
+            // 
+            // QuitButton
+            // 
+            this.QuitButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.QuitButton.Location = new System.Drawing.Point(12, 287);
+            this.QuitButton.Name = "QuitButton";
+            this.QuitButton.Size = new System.Drawing.Size(178, 32);
+            this.QuitButton.TabIndex = 0;
+            this.QuitButton.Text = "&Quit";
+            this.QuitButton.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(202, 251);
+            this.ClientSize = new System.Drawing.Size(202, 331);
+            this.ControlBox = false;
             this.Controls.Add(this.AsyncForms1Button);
             this.Controls.Add(this.AsyncFruitCountingButton);
+            this.Controls.Add(this.QuitButton);
+            this.Controls.Add(this.ParallelCodeButton);
+            this.Controls.Add(this.LocksButton);
             this.Controls.Add(this.EAPButton);
+            this.Controls.Add(this.FakeConsoleAppButton);
             this.Controls.Add(this.APMButton);
+            this.MinimumSize = new System.Drawing.Size(220, 380);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -103,6 +160,10 @@
         private System.Windows.Forms.Button AsyncForms1Button;
         private System.Windows.Forms.Button AsyncFruitCountingButton;
         private System.Windows.Forms.Button EAPButton;
+        private System.Windows.Forms.Button FakeConsoleAppButton;
+        private System.Windows.Forms.Button LocksButton;
+        private System.Windows.Forms.Button ParallelCodeButton;
+        private System.Windows.Forms.Button QuitButton;
     }
 }
 
